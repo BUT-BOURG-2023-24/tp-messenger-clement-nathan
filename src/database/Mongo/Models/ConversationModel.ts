f71012c00ec6f2af2744de128cd1bfc51b514cb9
Nathan Ozimek <nathozimek@gmail.com>
2023-10-17 11:46:10 +0200

    [ADD] ConversationModel

--- src/database/Mongo/Models/ConversationModel.ts
+++ src/database/Mongo/Models/ConversationModel.ts
@@ -1,12 +1,41 @@
-import mongoose, { Schema, Document } from "mongoose";
-import { MongooseID } from "../../../types";
+import mongoose, {Schema, Document, Types} from "mongoose";
+import UserModel, {IUser} from "./UserModel";
+import {IMessage} from "./MessageModel";
 
 export interface IConversation extends Document {
-	//A COMPLETER
+	participants: Types.DocumentArray<IUser>
+    messages: Types.DocumentArray<IMessage>
+    title: string
+    lastUpdate: Date
+    seen? : {
+        userId: number
+        messageId: number
+    }
 }
 
 const conversationSchema: Schema<IConversation> = new Schema<IConversation>({
-	//A COMPLETER
+	participants: {
+        type: [Schema<IUser>],
+        required: true
+    },
+    messages: {
+        type: [Schema<IMessage>],
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    lastUpdate: {
+        type: Date,
+        required: true
+    },
+    seen: {
+        type: new Schema({
+            userId: {type: Number},
+            messageId: {type: Number}
+        })
+    }
 });
 
 const ConversationModel = mongoose.model<IConversation>("Conversation", conversationSchema);
