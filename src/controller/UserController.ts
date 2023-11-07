1a3702b2fc5ceb87898804a2b12f2d9341f6a02d
Nathan Ozimek <nathozimek@gmail.com>
2023-11-07 16:46:56 +0100

    [ADD] UserController complet

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -1,6 +1,64 @@
 const UserModel = require('../database/Mongo/Models/UserModel');
 
-async function createUser()
+async function createUser(username: string, password: string, profilePicId?: number)
 {
-    let user = UserModel;
+    const user = new UserModel({
+        username: username,
+        password: password,
+        profilePicId: profilePicId
+    });
+    try
+    {
+        await user.save();
+    }
+    catch (error)
+    {
+        return error;
+    }
+}
+
+async function getUserByName(name: string)
+{
+    try {
+        const user = await UserModel.find(name);
+        return user;
+    }
+    catch (error)
+    {
+        return error;
+    }
+}
+
+async function getUserById(id: number)
+{
+    try {
+        const user = await UserModel.find(id);
+        return user;
+    }
+    catch (error)
+    {
+        return error;
+    }
+}
+
+async function getUsersByIds()
+{
+    try {
+        let users:any = [];
+        for (const id in arguments) {
+            users.push(UserModel.find(id));
+        }
+        return users;
+    }
+    catch (error)
+    {
+        return error;
+    }
+}
+
+module.exports = {
+    createUser,
+    getUserByName,
+    getUserById,
+    getUsersByIds
 }
